a761ccfc88432fe54e0e9bdc94e3444aac23365b
LSWarss <ls.warss98@gmail.com>
2021-04-15 07:21:09 +0200

    Columns transpositon implementation

--- cipher/cipher.py
+++ cipher/cipher.py
@@ -1,5 +1,5 @@
 import re
-
+import random
 
 def cipher_text(path):
     f = open(path)
@@ -8,12 +8,20 @@ def cipher_text(path):
     for i in range(len(chunksArray)): 
         if(i % 2 == 0): 
             chunksArray[i] = transposition_row(chunksArray[i])
-        
+        if(i % 3 == 0):
+            chunksArray[i] = transposition_column(chunksArray[i],3)
+    
+    print(  )
     return chunksArray
 
 def transposition_row(row : str): 
     assert type(row) == str
     return row[::-1]
+
+def transposition_column(row: str,transpostion_steps: int):
+    assert type(row) == str
+    column_number = random.randint(0, len(row))
+    return row.replace(row[column_number], row[column_number+transpostion_steps]).replace(row[column_number+transpostion_steps], row[column_number]) if column_number + transpostion_steps < len(row) else row
     
     
 
--- tests/cipher_test.py
+++ tests/cipher_test.py
@@ -1,7 +1,9 @@
-from cipher.cipher import cipher_text, transposition_row
+from cipher.cipher import cipher_text, transposition_row, transposition_column
 
 test_string = "LAMPA"
 
 def test_transpositon_row():
     assert transposition_row(test_string) == "APMAL"
 
+def test_transposition_column():
+    assert transposition_column(test_string,3)
